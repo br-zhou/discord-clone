b5a9ee222eacf28a2ee2aaa0703000301573f911
Brian Zhou <brianhzhou@gmail.com>
2023-08-13 16:16:47 -0700

    update app ui

--- client/src/pages/AppPage/AppPage.js
+++ client/src/pages/AppPage/AppPage.js
@@ -13,15 +13,28 @@ const AppPage = () => {
         <RoomIcon />
         <RoomIcon />
       </div>
-      <div className={classes.chat_container}>
-        <div>
+
+      <div className={classes.main_container}>
+        <div className={classes.server_title}>
+          <h2>Server Title</h2>
+        </div>
+
+        <div className={classes.chat_container}>
           <Message msg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis neque dui, efficitur sit amet est quis, cursus malesuada justo. Aliquam mauris velit, scelerisque vel nulla quis, ornare congue leo. Donec placerat congue mollis. Vestibulum quam eros, porta in maximus et, vulputate rutrum elit. Donec laoreet ipsum vel maximus semper. Cras sollicitudin in justo at placerat. Suspendisse blandit posuere mauris. In id aliquet ante. Proin ac tincidunt odio. Nulla molestie, quam vitae maximus tristique, nisi tellus faucibus dui, nec aliquet orci orci at massa. Pellentesque ultricies mauris tempor tellus tristique volutpat. Vivamus pharetra libero vel tincidunt imperdiet. Praesent pellentesque sed neque et dictum. Integer augue leo, tempor eget consequat ac, ornare eu risus." />
+          <Message msg="Lorem ipsum dolo" />
+          <Message msg="Lorem ipsum dolo" />
+          <Message msg="Lorem ipsum dolo" />
+          <Message msg="Lorem ipsum dolo" />
+          <Message msg="Lorem ipsum dolo" />
+          <Message msg="Lorem ipsum dolo" />
+          <Message msg="Lorem ipsum dolo" />
           <Message msg="Lorem ipsum dolo" />
         </div>
         <div>
           <ChatBox />
         </div>
       </div>
+
       <div className={classes.users}>
         <h3>Members:</h3>
         <Member />
