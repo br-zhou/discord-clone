6ba9efd856d95b141a018c8a1f7fccfdc07df320
Brian Zhou <brianhzhou@gmail.com>
2023-08-13 15:26:20 -0700

    add global store

---
+++ client/src/hooks/useStore.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+let globalStore = {
+  username: "Guest",
+  room: "General",
+};
+
+let listeners = [];
+
+export const useStore = () => {
+  const setState = useState(globalStore)[1];
+
+  const setStore = (name, value) => {
+    const newStore = { [name]: value };
+    globalStore = { ...globalStore, ...newStore };
+
+    for (const listener of listeners) {
+      listener(globalStore);
+    }
+
+    console.log(globalStore, listeners.length);
+  };
+
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.filter((li) => li !== setState);
+    };
+  }, []);
+
+  return [globalStore, setStore];
+};
